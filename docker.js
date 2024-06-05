08f90422e102bb79fe1e042556188d5af1ba93d3
yuanwangokk-1 <161530071+yuanwangokk-1@users.noreply.github.com>
2024-06-06 00:53:01 +0800

    Add files via upload

---
+++ docker.js
@@ -0,0 +1,15 @@
+docker管理器：https://docker.ariesver.shop/
+
+京东自动刷金豆：https://jd.ariesver.shop/
+
+订阅在线转换：https://sub.ariesver.shop/
+
+IT工具箱：https://tools.ariesver.shop/
+
+speedtest网址：https://v2.ariesver.shop/
+
+订阅转换后端地址：https://subconverter.ariesver.shop/sub?
+
+服务器监控：https://kuma.ariesver.shop/
+
+GitHub 文件加速：https://proxy.ariesver.shop/
